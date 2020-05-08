bbe70d4b1196dac0a067410189762547feaae666
maxonemore <maxonemore@ya.ru>
2020-05-08 20:30:20 +0300

    Третий пошел

---
+++ js.js
@@ -0,0 +1,7 @@
+'use strict';
+
+if (4*2 == 9){
+    console.log("Верно!");
+} else {
+    console.log("");
+}
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 'use strict';
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let expa = prompt("Введите обязательную статью расходов в этом месяце", "");
+let expb = Number(prompt("Во сколько обойдется?", ""));
+let expc = prompt("Введите обязательную статью расходов в этом месяце", "");
+let expd = Number(prompt("Во сколько обойдется?", ""));
 
-let persone = {
-	name: "John",
-	age: 35,
-	is_maried: false 
 
-}
+let appData = {
+	budget: [money],
+	timeData: [time],
+	expenses: {
+		expa: expb,
+		expc: expd		
+	},
+	optionalExpenses: "",
+	income: "",
+	savings: ""
+};
 
-console.log(persone.name);
+let dayBudget = (Number(appData.budget) - (appData.expenses.expa + appData.expenses.expc))/30;
 
-let arr = ['55', '66', '77'];
-
-console.log(arr[1]);
+console.log("Бюджет на день: " + dayBudget);
