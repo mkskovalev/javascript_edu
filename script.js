d94c205d0183e74035d6079107135499dde9d52f
maxonemore <maxonemore@ya.ru>
2020-05-08 17:24:19 +0300

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let persone = {
+	name: "John",
+	age: 35,
+	is_maried: false 
+
+}
+
+console.log(persone.name);
+
+let arr = ['55', '66', '77'];
+
+console.log(arr[1]);
